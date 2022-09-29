8573991caba670d2cc685c58e9a25876952d1cdf
Arnaud Triolet <contact@arnaudtriolet.fr>
2022-09-29 03:45:16 +0200

    Dark mode compatibility with gnome

--- source/linux/background-managers/gnome.js
+++ source/linux/background-managers/gnome.js
@@ -4,21 +4,42 @@ export async function isAvailable() {
 	return commandExists('gsettings');
 }
 
+export async function isDarkTheme() {
+	const {themeStyle} = await execFile('gsettings', [
+		'get',
+		'org.gnome.desktop.interface',
+		'color-scheme',
+	]);
+
+	return Boolean(themeStyle === 'prefer-dark');
+}
+
 export async function get() {
+	const prop = (isDarkTheme()) ? 'picture-uri-dark' : 'picture-uri';
+
 	const {stdout} = await execFile('gsettings', [
 		'get',
 		'org.gnome.desktop.background',
-		'picture-uri',
+		prop,
 	]);
 
 	return stdout.trim().slice(8, -1);
 }
 
 export async function set(imagePath) {
-	await execFile('gsettings', [
-		'set',
-		'org.gnome.desktop.background',
-		'picture-uri',
-		`file://${imagePath}`,
-	]);
+	if (isDarkTheme()) {
+		await execFile('gsettings', [
+			'set',
+			'org.gnome.desktop.background',
+			'picture-uri-dark',
+			`file://${imagePath}`,
+		]);
+	} else {
+		await execFile('gsettings', [
+			'set',
+			'org.gnome.desktop.background',
+			'picture-uri',
+			`file://${imagePath}`,
+		]);
+	}
 }
