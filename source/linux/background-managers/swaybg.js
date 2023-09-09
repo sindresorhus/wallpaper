11f0fdb307b09d5a05deb28b43e5bd3408f6c17c
VardanHeroic <tarayanvardan@gmail.com>
2023-09-09 10:05:40 +0400

    lint fix

--- source/linux/background-managers/swaybg.js
+++ source/linux/background-managers/swaybg.js
@@ -1,19 +1,9 @@
-import {commandExists} from '../util.js';
-import { spawn } from 'node:child_process'
+import {commandExists, spawn} from '../util.js';
 
 export async function isAvailable() {
 	return commandExists('swaybg');
 }
 
 export async function set(imagePath) {
-	let swaybg = spawn('swaybg', ['-i', imagePath, '-m', 'fill']);
-
-	swaybg.stderr.on('data', (data) => {
-		console.error(`${data}`);
-	});
-
-	swaybg.on('close', (code) => {
-		console.log(`swaybg exited with code ${code}`);
-	}); 
-
+	await spawn('swaybg', ['-i', imagePath, '-m', 'fill']);
 }
