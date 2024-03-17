ca94ed568bc897b0b6251099433d838464cceac1
xero08 <anuvab.spd@gmail.com>
2024-03-17 21:47:54 +0530

    Fix for dark style on Gnome

--- source/linux/background-managers/gnome.js
+++ source/linux/background-managers/gnome.js
@@ -1,24 +1,36 @@
-import {commandExists, execFile} from '../util.js';
+import {commandExists, execFile, hasLine} from '../util.js';
 
 export async function isAvailable() {
 	return commandExists('gsettings');
 }
 
+async function isDarkStyle() {
+	const {stdout} = await execFile('gsettings', [
+		'get',
+		'org.gnome.desktop.interface',
+		'color-scheme'
+	]);
+	return hasLine(stdout, "'prefer-dark'") ? true : false;
+}
+
 export async function get() {
+	const style = (await isDarkStyle()) ? 'picture-uri-dark' : 'picture-uri';
+
 	const {stdout} = await execFile('gsettings', [
 		'get',
 		'org.gnome.desktop.background',
-		'picture-uri',
+		style,
 	]);
 
 	return stdout.trim().slice(8, -1);
 }
 
 export async function set(imagePath) {
+	const style = (await isDarkStyle()) ? 'picture-uri-dark' : 'picture-uri';
 	await execFile('gsettings', [
 		'set',
 		'org.gnome.desktop.background',
-		'picture-uri',
+		style,
 		`file://${imagePath}`,
 	]);
 }
