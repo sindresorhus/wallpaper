d3b90d9d32ac541d7f816c2fd4b6b9eb988f110b
Anuvab <anuvab.spd@gmail.com>
2024-03-18 16:59:46 +0530

    Fix setting wallpaper for dark style on Gnome (#96)
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>

--- source/linux/background-managers/gnome.js
+++ source/linux/background-managers/gnome.js
@@ -1,24 +1,38 @@
-import {commandExists, execFile} from '../util.js';
+import {commandExists, execFile, hasLine} from '../util.js';
 
 export async function isAvailable() {
 	return commandExists('gsettings');
 }
 
+async function isDarkStyle() {
+	const {stdout} = await execFile('gsettings', [
+		'get',
+		'org.gnome.desktop.interface',
+		'color-scheme',
+	]);
+
+	return Boolean(hasLine(stdout, '\'prefer-dark\''));
+}
+
 export async function get() {
+	const keyForStyle = (await isDarkStyle()) ? 'picture-uri-dark' : 'picture-uri';
+
 	const {stdout} = await execFile('gsettings', [
 		'get',
 		'org.gnome.desktop.background',
-		'picture-uri',
+		keyForStyle,
 	]);
 
 	return stdout.trim().slice(8, -1);
 }
 
 export async function set(imagePath) {
+	const keyForStyle = (await isDarkStyle()) ? 'picture-uri-dark' : 'picture-uri';
+
 	await execFile('gsettings', [
 		'set',
 		'org.gnome.desktop.background',
-		'picture-uri',
+		keyForStyle,
 		`file://${imagePath}`,
 	]);
 }
